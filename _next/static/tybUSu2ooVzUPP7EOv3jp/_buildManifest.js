94d0bf1514a83cf20d483fa177037be685f35c32
hikaru122700 <hikaru122700@gmail.com>
2026-01-09 02:00:40 +0900

    Updates

---
+++ _next/static/tybUSu2ooVzUPP7EOv3jp/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/shadobus//_next/:path+",
+        "destination": "/shadobus/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
